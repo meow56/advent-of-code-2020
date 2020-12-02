348d43a7bfc86ede94362b52f4a5c8830af342e5
meow56 <spinattacktotheface@gmail.com>
2020-12-01 18:17:06 -0600

    day1 final
    Cleaned up the code, added a few comments, outputs both solutions.

--- day 1/day1.js
+++ day 1/day1.js
@@ -3,30 +3,29 @@
 const inputElement = document.getElementById("input");
 inputElement.addEventListener("change", handleFiles, false);
 function handleFiles() {
-  const input = this.files[0];
-  input.text().then(text => doStuff(text));
+	const input = this.files[0];
+	input.text().then(text => doStuff(text));
 
-  function doStuff(text) {
-  	let regex = /\b\d+\b/g;
-  	let entry = regex.exec(text);
-  	let numbers = [];
-  	while(entry) {
-  		numbers.push(entry);
-  		entry = regex.exec(text);
-  		if(numbers.length > 1000) {
-  			throw "loop too long";
-  		}
-  	}
-  	console.log(numbers);
-  	for(let i = 0; i < numbers.length; i++) {
-  		for(let j = i + 1; j < numbers.length; j++) {
-  			for(let k = j + 1; k < numbers.length; k++) {
-	  			if(Number(numbers[i][0]) + Number(numbers[j][0]) + Number(numbers[k][0]) === 2020) {
-	  				console.log(Number(numbers[i][0]) * Number(numbers[j][0]) * Number(numbers[k][0]));
-	  			}
-	  		}
-  		}
-  	}
-  }
+	function doStuff(text) {
+		let regex = /\b\d+\b/g; // "find all sequences of the form line-break (number) line-break"
+		let entry = regex.exec(text);
+		let numbers = [];
+		while(entry) { // loop over the text with that regex
+			numbers.push(entry);
+			entry = regex.exec(text);
+		}
+		for(let i = 0; i < numbers.length; i++) {
+			for(let j = i + 1; j < numbers.length; j++) {
+				if(Number(numbers[i][0]) + Number(numbers[j][0])=== 2020) {
+					console.log(Number(numbers[i][0]) * Number(numbers[j][0])); // part 1
+				}
+				for(let k = j + 1; k < numbers.length; k++) {
+					if(Number(numbers[i][0]) + Number(numbers[j][0]) + Number(numbers[k][0]) === 2020) {
+						console.log(Number(numbers[i][0]) * Number(numbers[j][0]) * Number(numbers[k][0])); // part 2
+					}
+				}
+			}
+		}
+	}
 }
 
