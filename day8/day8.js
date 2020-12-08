730a7dc2513fdb39dde5fc41eb3d4572c7eae899
meow56 <spinattacktotheface@gmail.com>
2020-12-08 15:58:02 -0600

    Day 8-2
    Part 2 of day 8. Don't you just love brute force? And throwing because I don't know how to cut out of forEach?

--- day8/day8.js
+++ day8/day8.js
@@ -9,32 +9,57 @@ function day8(text) {
 		entry = regex.exec(text);
 	}
 
-	let instPointer = 0;
-	let accumulator = 0;
-	let pastVisits = [];
-	let dontStop = true;
+	function runCode(code) {
+		let instPointer = 0;
+		let accumulator = 0;
+		let pastVisits = [];
+		let dontStop = true;
 
-	while(dontStop) {
-		pastVisits.forEach(function(visit) {
-			if(instPointer === visit) { 
-				console.log(accumulator); 
-				dontStop = false; 
+		while(dontStop) {
+			pastVisits.forEach(function(visit) {
+				if(instPointer === visit) { 
+					console.log("Infinite loop occured, accumulator: " + accumulator); 
+					dontStop = false;
+				}
+			});
+			pastVisits.push(instPointer);
+
+			if(instPointer >= code.length) {
+				console.log("Program terminated, accumulator: " + accumulator);
+				dontStop = false;
+				return "done";
 			}
-		});
-		pastVisits.push(instPointer);
+			let curInst = code[instPointer][1];
+			let curNum = Number(code[instPointer][2]);
 
-		let curInst = instructions[instPointer][1];
-		let curNum = Number(instructions[instPointer][2]);
-
-		switch(curInst) {
-			case "acc":
-				accumulator += curNum;
-			case "nop":
-				instPointer++;
-				break;
-			case "jmp":
-				instPointer += curNum;
+			switch(curInst) {
+				case "acc":
+					accumulator += curNum;
+				case "nop":
+					instPointer++;
+					break;
+				case "jmp":
+					instPointer += curNum;
+					break;
+			}
 		}
-
 	}
+	runCode(instructions);
+	instructions.forEach(function(inst, index) { // good ol' brute force saving the day
+		if(inst[1] === "jmp") {
+			let instCopy = instructions.slice();
+			instCopy[index] = [inst[0], "nop", inst[2]];
+			let done = runCode(instCopy);
+			if(done === "done") {
+				throw "Screw this, I'm outta here!";
+			}
+		} else if(inst[1] === "nop") {
+			let instCopy = instructions.slice();
+			instCopy[index] = [inst[0], "jmp", inst[2]];
+			let done = runCode(instCopy);
+			if(done === "done") {
+				throw "Screw this, I'm outta here!";
+			}
+		}
+	}) 
 }
