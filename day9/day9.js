156e372123ef32746eb9a859f16b0c7022af4747
meow56 <spinattacktotheface@gmail.com>
2020-12-09 22:10:05 -0600

    Day 9-2
    Part 2, day 9. BOY DO I LOVE FORGETTING REGEXS RETURN STRINGS. Also misreading the instructions and being confused when adding the first and last elements together didn't work.

--- day9/day9.js
+++ day9/day9.js
@@ -9,12 +9,14 @@ function day9(text) {
 		entry = regex.exec(text);
 	}
 
+	const weakNumber = 26134589;
 	for(let i = 25; i < numbers.length; i++) {
 		let priorNumbers = numbers.slice(i - 25, i);
 		if(findSum(priorNumbers, numbers[i]) === "oh") {
 			console.log(numbers[i]); // part 1
 		}
 	}
+	console.log(weakSum()); // part 2
 
 	function findSum(numbers, sum) {
 		for(let i = 0; i < numbers.length; i++) {
@@ -25,5 +27,36 @@ function day9(text) {
 			}
 		}
 		return "oh";
+	}
+
+	function weakSum() {
+		let lowerBound = 0;
+		let upperBound = 1;
+		while(true) {
+			if(upperBound > numbers.length) {
+				throw "Upper Bound exceeds array length";
+			}
+			let sum = 0;
+			for(let i = lowerBound; i < upperBound; i++) {
+				sum += Number(numbers[i]);
+			}
+			if(sum === weakNumber) {
+				let lowest = 1000000000000000;
+				let highest = 0;
+				for(let i = lowerBound; i < upperBound; i++) {
+					if(Number(numbers[i]) < lowest) {
+						lowest = Number(numbers[i]);
+					}
+					if(Number(numbers[i]) > highest) {
+						highest = Number(numbers[i]);
+					}
+				}
+				return lowest + highest;
+			} else if(sum > weakNumber) {
+				lowerBound++;
+			} else {
+				upperBound++;
+			}
+		}
 	}
 }
