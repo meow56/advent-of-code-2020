0cfc4305f3ef29ffd3581a5f12c8aa1b00b64712
meow56 <spinattacktotheface@gmail.com>
2020-12-12 13:38:39 -0600

    Day 12-2
    Day 12, part 2. Ah, the classic programmer adage--"I made this code but I have no idea how it works". I honestly can't believe this worked out perfectly, I was so sure something was wrong.

--- day12/day12.js
+++ day12/day12.js
@@ -40,5 +40,60 @@ function day12(text) {
 		}
 	});
 
-	console.log(Math.abs(NS) + Math.abs(EW));
+	console.log(Math.abs(NS) + Math.abs(EW)); // part 1
+
+
+	let trueNS = 0;
+	let trueEW = 0;
+	let wayNS = 1;
+	let wayEW = 10;
+	let wayFace = [0, 1];
+	const wayCompass = [
+		1, 1, -1, -1
+	];
+
+	directions.forEach(function(direction) {
+		let trueDir = direction[0];
+		let workWayNS = wayNS;
+		let workWayEW = wayEW;
+		wayFace = [(wayNS > 0 ? 0 : 2), (wayEW > 0 ? 1 : 3)];
+		let turning = direction[1] / 90;
+		let newWayFace;
+		switch(trueDir) {
+			case "N":
+				wayNS += direction[1];
+				break;
+			case "E":
+				wayEW += direction[1];
+				break;
+			case "S":
+				wayNS -= direction[1];
+				break;
+			case "W":
+				wayEW -= direction[1];
+				break;
+			case "L":
+				for(let i = 0; i < turning; i++) {
+					workWayNS = wayCompass[wayFace[1]] * wayCompass[(((wayFace[1] - 1) % 4) + 4) % 4] * wayEW;
+					workWayEW = wayCompass[wayFace[0]] * wayCompass[(((wayFace[0] - 1) % 4) + 4) % 4] * wayNS;
+					wayNS = workWayNS;
+					wayEW = workWayEW;
+				}
+				break;
+			case "R":
+				for(let i = 0; i < turning; i++) {
+					workWayNS = wayCompass[wayFace[1]] * wayCompass[(((wayFace[1] + 1) % 4) + 4) % 4] * wayEW;
+					workWayEW = wayCompass[wayFace[0]] * wayCompass[(((wayFace[0] + 1) % 4) + 4) % 4] * wayNS;
+					wayNS = workWayNS;
+					wayEW = workWayEW;
+				}
+				break;
+			case "F":
+				trueNS += wayNS * direction[1];
+				trueEW += wayEW * direction[1];
+				break;
+		}
+	});
+
+	console.log(Math.abs(trueNS) + Math.abs(trueEW)); // part 2
 }
