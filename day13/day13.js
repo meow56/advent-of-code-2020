ab4cad2776010f7ec46869c3985d7eaf801b18a3
meow56 <spinattacktotheface@gmail.com>
2020-12-13 11:36:08 -0600

    Day 13-2
    Day 13, part 2. Hey, that was pretty fun! Had to think a while for that one. And I started with a brute force solution that obviously didn't work--turns out you can't make an array of length ~1e14.

--- day13/day13.js
+++ day13/day13.js
@@ -1,23 +1,42 @@
 "use strict";
 
 function day13(text) {
-	let regex = /[0-9]+/gm;
+	let regex = /[0-9]+|x,/gm;
 	let entry = regex.exec(text);
+	let firstNum = +entry[0];
+	entry = regex.exec(text);
 	let buses = [];
 	while(entry) { // loop over the text with that regex
-		buses.push(Number(entry[0]));
+		buses.push(entry[0]);
 		entry = regex.exec(text);
 	}
 
 	let times = [];
 	let lowestTime = 100000000;
 	let lowestID = 0;
-	for(let i = 1; i < buses.length; i++) {
-		times[i] = Math.ceil(buses[0] / buses[i]) * buses[i];
-		if(times[i] < lowestTime) {
-			lowestTime = times[i];
-			lowestID = buses[i];
+	for(let i = 0; i < buses.length; i++) {
+		if(buses[i] !== "x,") {
+			times[i] = Math.ceil(firstNum / +buses[i]) * +buses[i];
+			if(times[i] < lowestTime) {
+				lowestTime = times[i];
+				lowestID = +buses[i];
+			}
 		}
 	}
-	console.log((lowestTime - buses[0]) * lowestID); // part 1
+	console.log((lowestTime - firstNum) * lowestID); // part 1
+
+	let base = 1;
+	let number = 0;
+	for(let i = 0; i < buses.length; i++) {
+		if(buses[i] === "x,") {
+			continue;
+		}
+
+		while((number + i) % +buses[i] !== 0) {
+			number += base;
+		}
+		base *= +buses[i];
+		console.log("Finished with bus " + buses[i]);
+	}
+	console.log(number); // part 2
 }
