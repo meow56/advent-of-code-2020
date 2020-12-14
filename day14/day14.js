bb96a978d7d85de32e1c4369161a3856252c2ff6
meow56 <spinattacktotheface@gmail.com>
2020-12-14 17:31:21 -0600

    Day 14-2
    Day 14, part 2.
    
    *uses previous solution to sum up all the numbers*
    "You know, this is pretty slow. Maybe I should use a different solution, like with Object.keys() instead."
    *gets answer*
    "Oh, ok. Might as well try it."
    *inputs answer; it's wrong*
    "Welp, that's too bad. Anyways, let's implement that Object.keys() thing I was talking about."
    "Ok, implemented. Let me just make sure it works..."
    "Hey, wait, this number is different than the previous number. What? I haven't changed anything else..."
    *inputs answer; it's correct*
    "????"

--- day14/day14.js
+++ day14/day14.js
@@ -35,6 +35,24 @@ function day14(text) {
 	}
 	console.log(finalSum); // part 1
 
+	let trueMem = [];
+
+	instructions.forEach(function(inst) {
+		let type = inst[0];
+		if(type === "mask") {
+			curMask = inst[1];
+		} else {
+			let bin = decimalToBinary(inst[0]);
+			bin = applyBitmask(bin, curMask, true);
+			bin.forEach(address => trueMem[binaryToDecimal(address)] = inst[1]);
+		}
+	});
+
+	let finalSum2 = 0;
+	let defined = Object.keys(trueMem);
+	defined.forEach(index => finalSum2 += trueMem[index]);
+	console.log(finalSum2); // part 1
+
 	function decimalToBinary(num) {
 		/*
 			Converts an integer into a 36-length binary string.
@@ -78,18 +96,40 @@ function day14(text) {
 		return sum;
 	}
 
-	function applyBitmask(num, mask) {
+	function applyBitmask(num, mask, part2 = false, index = 0) {
 		if(num.length !== mask.length) {
 			throw "Number and mask are not the same length.";
 		}
-		let result = num;
-		for(let i = 0; i < mask.length; i++) {
-			if(mask[i] === "1") {
-				result = result.slice(0, i) + "1" + result.slice(i + 1);
-			} else if(mask[i] === "0") {
-				result = result.slice(0, i) + "0" + result.slice(i + 1);
+		if(!part2) {
+			let result = num;
+			for(let i = 0; i < mask.length; i++) {
+				if(mask[i] === "1") {
+					result = result.slice(0, i) + "1" + result.slice(i + 1);
+				} else if(mask[i] === "0") {
+					result = result.slice(0, i) + "0" + result.slice(i + 1);
+				}
 			}
+			return result;
+		} else {
+			if(devMode) {
+				console.log("Number " + num + " with mask " + mask + ". Current index: " + index);
+			}
+			let working = num;
+			let result = [];
+			for(let i = index; i < mask.length; i++) {
+				if(mask[i] === "1") {
+					working = working.slice(0, i) + "1" + working.slice(i + 1);
+				} else if(mask[i] === "X") {
+					let answer0 = applyBitmask(working.slice(0, i) + "0" + working.slice(i + 1), mask, true, i + 1);
+					let answer1 = applyBitmask(working.slice(0, i) + "1" + working.slice(i + 1), mask, true, i + 1);
+					answer0.forEach(res => result.push(res));
+					answer1.forEach(res => result.push(res));
+				}
+			}
+			if(result.length === 0) {
+				result = [working];
+			}
+			return result;
 		}
-		return result;
 	}
 }
