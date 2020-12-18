4895a6041c4376789ffae11f5934e2e2156aa266
meow56 <spinattacktotheface@gmail.com>
2020-12-17 18:24:49 -0600

    Day 17-2
    Day 17, part 2. Finally, I can partake in the memes. "My program takes 30 seconds to finish!" "30 seconds? My program takes a minute!" "A minute?? My program takes 20 minutes!" "Your programs terminate!?"

--- day17/day17.js
+++ day17/day17.js
@@ -9,9 +9,12 @@ function day17(text) {
 		entry = regex.exec(text);
 	}
 
-	let grid = [cubes.map(function(cube) {
+	let grid = [[cubes.map(function(cube) {
 		return cube.split("");
-	})]; // z, y, x
+	})]]; // w, z, y, x
+
+	let storeGrid = grid.slice();
+	grid = grid[0]; // for part 1
 
 	function life(x, y, z) {
 		let aliveSum = 0;
@@ -82,6 +85,120 @@ function day17(text) {
 		}
 	}
 
+	function life4(x, y, z, w) {
+		let aliveSum = 0;
+		const directions = [
+			[ // hyper-low (w)
+				[ // lower (z)
+					//      left             mid             right
+					[[-1, -1, -1, -1], [0, -1, -1, -1], [1, -1, -1, -1]], // up
+					[[-1,  0, -1, -1], [0,  0, -1, -1], [1,  0, -1, -1]], // mid
+					[[-1,  1, -1, -1], [0,  1, -1, -1], [1,  1, -1, -1]]  // down
+				],
+				[ // mid (z)
+					//      left             mid             right
+					[[-1, -1,  0, -1], [0, -1,  0, -1], [1, -1,  0, -1]], // up
+					[[-1,  0,  0, -1], [0,  0,  0, -1], [1,  0,  0, -1]], // mid
+					[[-1,  1,  0, -1], [0,  1,  0, -1], [1,  1,  0, -1]]  // down
+				],
+				[ // upper (z)
+					//      left             mid             right
+					[[-1, -1,  1, -1], [0, -1,  1, -1], [1, -1,  1, -1]], // up
+					[[-1,  0,  1, -1], [0,  0,  1, -1], [1,  0,  1, -1]], // mid
+					[[-1,  1,  1, -1], [0,  1,  1, -1], [1,  1,  1, -1]]  // down
+				]
+			],
+			[ // hyper-mid (w)
+				[ // lower (z)
+					//      left             mid             right
+					[[-1, -1, -1, 0], [0, -1, -1, 0], [1, -1, -1, 0]], // up
+					[[-1,  0, -1, 0], [0,  0, -1, 0], [1,  0, -1, 0]], // mid
+					[[-1,  1, -1, 0], [0,  1, -1, 0], [1,  1, -1, 0]]  // down
+				],
+				[ // mid (z)
+					//      left             mid             right
+					[[-1, -1,  0, 0], [0, -1,  0, 0], [1, -1,  0, 0]], // up
+					[[-1,  0,  0, 0], [0,  0,  0, 0], [1,  0,  0, 0]], // mid
+					[[-1,  1,  0, 0], [0,  1,  0, 0], [1,  1,  0, 0]]  // down
+				],
+				[ // upper (z)
+					//      left             mid             right
+					[[-1, -1,  1, 0], [0, -1,  1, 0], [1, -1,  1, 0]], // up
+					[[-1,  0,  1, 0], [0,  0,  1, 0], [1,  0,  1, 0]], // mid
+					[[-1,  1,  1, 0], [0,  1,  1, 0], [1,  1,  1, 0]]  // down
+				]
+			],
+			[ // hyper-high (w)
+				[ // lower (z)
+					//      left             mid             right
+					[[-1, -1, -1, 1], [0, -1, -1, 1], [1, -1, -1, 1]], // up
+					[[-1,  0, -1, 1], [0,  0, -1, 1], [1,  0, -1, 1]], // mid
+					[[-1,  1, -1, 1], [0,  1, -1, 1], [1,  1, -1, 1]]  // down
+				],
+				[ // mid (z)
+					//      left             mid             right
+					[[-1, -1,  0, 1], [0, -1,  0, 1], [1, -1,  0, 1]], // up
+					[[-1,  0,  0, 1], [0,  0,  0, 1], [1,  0,  0, 1]], // mid
+					[[-1,  1,  0, 1], [0,  1,  0, 1], [1,  1,  0, 1]]  // down
+				],
+				[ // upper (z)
+					//      left             mid             right
+					[[-1, -1,  1, 1], [0, -1,  1, 1], [1, -1,  1, 1]], // up
+					[[-1,  0,  1, 1], [0,  0,  1, 1], [1,  0,  1, 1]], // mid
+					[[-1,  1,  1, 1], [0,  1,  1, 1], [1,  1,  1, 1]]  // down
+				]
+			]
+		];
+
+		try {
+			directions.forEach(function(wDir, wInd) {
+				wDir.forEach(function(zDir, zInd) {
+					zDir.forEach(function(yDir, yInd) {
+						yDir.forEach(function(xDir, xInd) {
+							if(xDir[0] !== 0 || xDir[1] !== 0 || xDir[2] !== 0 || xDir[3] !== 0) {
+								let workX = x + xDir[0];
+								let workY = y + xDir[1];
+								let workZ = z + xDir[2];
+								let workW = w + xDir[3];
+								let interestCube;
+								try {
+									interestCube = grid[workW][workZ][workY][workX][0];
+									if(interestCube === "#") {
+										aliveSum++;
+									}
+								} catch(e) {
+									// If there is an error, that means we've escaped the bounds.
+									// Since everything outside is empty, nothing needs to be added.
+								}
+								if(aliveSum > 3) {
+									throw "Too many cells"; // Me when you can't return from forEach so you decide to throw an error instead of refactoring the code
+								}
+							}
+						});
+					});
+				});
+			});
+		} catch(e) {
+			if(e === "Too many cells") {
+				return false;
+			} else {
+				throw e; // up to the top!
+			}
+		}
+
+		let currentCell;
+		try {
+			currentCell = grid[w][z][y][x];
+		} catch(e) {
+			currentCell = ".";
+		}
+		if(currentCell === "#") {
+			return aliveSum === 2 || aliveSum === 3;
+		} else {
+			return aliveSum === 3;
+		}
+	}
+
 	function step() {
 		let workGrid = [];
 		for(let z = -1; z <= grid.length; z++) {
@@ -98,6 +215,28 @@ function day17(text) {
 		}
 
 		grid = workGrid;
+	}
+
+	function step4() {
+		let workHypergrid = [];
+		for(let w = -1; w <= grid.length; w++) {
+			let workGrid = [];
+			for(let z = -1; z <= grid[0].length; z++) {
+				let workPlane = [];
+				for(let y = -1; y <= grid[0][0].length; y++) { // they're all the same length, it should be ok
+					let workLine = [];
+					for(let x = -1; x <= grid[0][0][0].length; x++) { // ...right?
+						// -1 and length to expand the grid by one every step.
+						workLine.push(life4(x, y, z, w) ? "#" : ".");
+					}
+					workPlane.push(workLine);
+				}
+				workGrid.push(workPlane);
+			}
+			workHypergrid.push(workGrid);
+		}
+
+		grid = workHypergrid;
 	}
 
 	const iterations = 6;
@@ -133,5 +272,44 @@ function day17(text) {
 			return acc;
 		}
 	}, 0);
-	console.log(sum);
+	console.log(sum); // part 1
+
+	grid = storeGrid.slice();
+
+	displayText("step 0");
+	for(let w = 0; w < grid.length; w++) {
+		for(let z = 0; z < grid[w].length; z++) {
+			displayText(`z-index ${z}, w-index ${w}`);
+			for(let y = 0; y < grid[w][z].length; y++) {
+				displayText(grid[w][z][y].join(""));
+			}
+		}
+	}
+	displayText();
+
+	for(let i = 0; i < iterations; i++) {
+		console.time(`step4 ${i + 1}`);
+		step4();
+		console.timeEnd(`step4 ${i + 1}`);
+
+		displayText(`step4 ${i + 1}`);
+		for(let w = 0; w < grid.length; w++) {
+			for(let z = 0; z < grid[w].length; z++) {
+				displayText(`z-index ${z}, w-index ${w}`);
+				for(let y = 0; y < grid[w][z].length; y++) {
+					displayText(grid[w][z][y].join(""));
+				}
+			}
+		}
+		displayText();
+	}
+	let flatGrid4 = grid.flat(4);
+	let sum4 = flatGrid4.reduce(function(acc, value) {
+		if(value === "#") {
+			return acc + 1;
+		} else {
+			return acc;
+		}
+	}, 0);
+	console.log(sum4); // part 2
 }
