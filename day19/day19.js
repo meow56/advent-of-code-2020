027e3ef28d456c93801b42f57c90a11aef6d91ce
meow56 <spinattacktotheface@gmail.com>
2020-12-19 15:45:22 -0600

    Day 19-2
    Day 19, part 2. Ok I implemented the regex version and it's way faster. Amazing. Also check out the monster regex that it creates. Pretty great.

--- day19/day19.js
+++ day19/day19.js
@@ -35,62 +35,89 @@ function day19(text) {
 		messEntry = messageRegex.exec(text);
 	}
 
-
-	console.log(rules);
-
 	rules.sort((a, b) => +a[0] - +b[0]);
 
-	function evaluateRegex(regex) {
-		let validMessages = [];
-		if(regex.length === 3) {
-			if(regex[1].length === 2) {
-				let part1 = evaluateRegex(rules[regex[1][0]]);
-				let part2 = evaluateRegex(rules[regex[1][1]]);
-				for(let i = 0; i < part1.length; i++) {
-					for(let j = 0; j < part2.length; j++) {
-						validMessages.push(part1[i] + part2[j]);
-					}
-				}
+	function evalRuleBetter(rule) {
+		// This time, let's actually make a regex.
+		let finalRegex = "";
+		if(rule.length === 3) {
+			if(rule[1].length === 2) {
+				let part1 = evalRuleBetter(rules[rule[1][0]]);
+				let part2 = evalRuleBetter(rules[rule[1][1]]);
+				finalRegex = part1 + part2;
 			} else {
-				validMessages = evaluateRegex(rules[regex[1][0]]);
+				finalRegex = evalRuleBetter(rules[rule[1][0]]);
 			}
-			if(regex[2].length === 2) {
-				let part3 = evaluateRegex(rules[regex[2][0]]);
-				let part4 = evaluateRegex(rules[regex[2][1]]);
-				for(let i = 0; i < part3.length; i++) {
-					for(let j = 0; j < part4.length; j++) {
-						validMessages.push(part3[i] + part4[j]);
-					}
-				}
+			if(rule[2].length === 2) {
+				let part3 = evalRuleBetter(rules[rule[2][0]]);
+				let part4 = evalRuleBetter(rules[rule[2][1]]);
+				finalRegex = "(?:" + finalRegex + "|" + part3 + part4 + ")";
 			} else {
-				validMessages = [...validMessages, ...evaluateRegex(rules[regex[2][0]])];
+				finalRegex = "(?:" + finalRegex + "|" + evalRuleBetter(rules[rule[2][0]]) + ")";
 			}
 		} else {
-			if(regex[1] === "a" || regex[1] === "b") {
-				return [regex[1]];
+			if(rule[1] === "a" || rule[1] === "b") {
+				return rule[1];
 			}
-			if(regex[1].length === 2) {
-				let part1 = evaluateRegex(rules[regex[1][0]]);
-				let part2 = evaluateRegex(rules[regex[1][1]]);
-				for(let i = 0; i < part1.length; i++) {
-					for(let j = 0; j < part2.length; j++) {
-						validMessages.push(part1[i] + part2[j]);
-					}
-				}
+			if(rule[1].length === 2) {
+				let part1 = evalRuleBetter(rules[rule[1][0]]);
+				let part2 = evalRuleBetter(rules[rule[1][1]]);
+				finalRegex = part1 + part2;
 			} else {
-				validMessages = evaluateRegex(rules[regex[1][0]]);
+				finalRegex = evalRuleBetter(rules[rule[1][0]]);
 			}
 		}
-		return validMessages;
+		return finalRegex;
 	}
 
-	let valid = evaluateRegex(rules[0]);
+	let validRegex = RegExp("^" + evalRuleBetter(rules[0]) + "$");
+	console.log(validRegex);
 
-	let count = 0;
+	let validRegexMessages = [];
 	console.log(messages.reduce(function(acc, value) {
-		if(valid.includes(value)) {
+		if(validRegex.test(value)) {
 			acc++;
+			validRegexMessages.push(value);
 		}
 		return acc;
-	}, 0));
+	}, 0)); // part 1
+
+
+	let rule42 = RegExp(evalRuleBetter(rules[42]), "g");
+	let rule31 = RegExp(evalRuleBetter(rules[31]), "g");
+	console.log(rule42);
+	console.log(rule31);
+
+	console.log(messages.reduce(function(acc, value) {
+		let workingMessage = value.slice();
+		let rule42Count = [];
+		let rule31Count = [];
+		let lastIndex42 = 0;
+		let found = workingMessage.matchAll(rule42);
+		for(const match of found) {
+			if(match.index === lastIndex42) {
+				rule42Count.push(match[0]);
+				lastIndex42 = match.index + match[0].length;
+			}
+		}
+		rule31.lastIndex = lastIndex42;
+		found = workingMessage.matchAll(rule31);
+		let lastIndex31 = lastIndex42;
+		for(const match of found) {
+			if(match.index === lastIndex31) {
+				rule31Count.push(match);
+				lastIndex31 = match.index + match[0].length;
+			}
+		}
+		if(rule42Count.join("").length + rule31Count.join("").length === workingMessage.length) {
+			if(rule42Count.length > rule31Count.length && rule31Count.length !== 0) {
+				acc++;
+			}
+		}
+
+		rule42.lastIndex = 0;
+		rule31.lastIndex = 0;
+		return acc;
+	}, 0)); // part 2
+
 }
